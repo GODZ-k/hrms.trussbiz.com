1d0dd33a8027904952abe268789382aa83a8b93b
tanmay <tanmaykhatri07726@gmail.com>
2025-02-06 09:45:44 +0530

    change

--- src/app/api/v1/employee/post/route.ts
+++ src/app/api/v1/employee/post/route.ts
@@ -3,12 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { EmployeePersonalDetails, employeePersonalDetailsSchema } from "../types";
 
 export async function POST(req: NextRequest) {
-    const session = await auth();
-
-    if(!session) {
-        return NextResponse.json({ error: "Unauthorized" , success: false }, { status: 401 });
-    }
-
     const inputData:EmployeePersonalDetails = await req.json();
 
     const payload = employeePersonalDetailsSchema.parse(inputData);
