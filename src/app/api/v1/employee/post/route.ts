ae5ab6721523423fefe8cd94dffc394933894a1b
Tanmay/K and A Tech - MERN <tanmaykandatech-mern@192.168.1.20>
2025-02-06 09:57:19 +0530

    main

--- src/app/api/v1/employee/post/route.ts
+++ src/app/api/v1/employee/post/route.ts
@@ -11,5 +11,7 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ error: "Invalid input data" , success: false }, { status: 400 });
     }
 
+    return NextResponse.json({})
+
    
 }
